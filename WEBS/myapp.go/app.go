1961146011e7e850b697dc3670cb14114a5b277f
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-06-22 00:52:37 +0900

    rest API complete

--- WEBS/myapp.go/app.go
+++ WEBS/myapp.go/app.go
@@ -27,7 +27,20 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func usersHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Get UserInfo by /users/{id}")
+	if len(userMap) == 0 {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User")
+		return
+	}
+	users := []*User{}
+	for _, u := range userMap {
+		users = append(users, u)
+	}
+
+	data, _ := json.Marshal(users)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
 }
 
 func getUserInfo(w http.ResponseWriter, r *http.Request) {
@@ -70,6 +83,57 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	data, _ := json.Marshal(user) // json 형식으로 인코딩
 	fmt.Fprint(w, string(data))
+	fmt.Println(lastId)
+}
+
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	_, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User ID:", id)
+		return
+	}
+	delete(userMap, id) // map 원소 삭제는 delete()
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Delete User ID:", id)
+}
+
+func updateUserHandler(w http.ResponseWriter, r *http.Request) {
+	updateUser := new(User)
+	err := json.NewDecoder(r.Body).Decode(updateUser)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	user, ok := userMap[updateUser.ID]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User ID:", updateUser.ID)
+		return
+	}
+
+	if updateUser.FirstName != "" {
+		user.FirstName = updateUser.FirstName
+	}
+	if updateUser.LastName != "" {
+		user.LastName = updateUser.LastName
+	}
+	if updateUser.Email != "" {
+		user.Email = updateUser.Email
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+
 }
 
 // NewHandler make a new handler
@@ -79,8 +143,10 @@ func NewHandler() http.Handler {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", usersHandler).Methods("GET") // 고릴라 먹스 덕에 같은 url에서도 모드에 따라 역할을 분리시킬 수 있다.
 	mux.HandleFunc("/users", createUserHandler).Methods("POST")
-	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfo)
+	mux.HandleFunc("/users", updateUserHandler).Methods("PUT")
+	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfo).Methods("GET")
+	mux.HandleFunc("/users/{id:[0-9]+}", deleteUserHandler).Methods("DELETE")
 	return mux
 }
--- WEBS/myapp.go/app_test.go
+++ WEBS/myapp.go/app_test.go
@@ -2,6 +2,7 @@ package myapp
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -33,7 +34,8 @@ func TestUsers(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, res.StatusCode)
 	data, _ := ioutil.ReadAll(res.Body)
-	assert.Contains(string(data), "Get UserInfo") // 앞에 있는 놈이 뒤에 있는 놈을 포함해야 OK
+	assert.Contains(string(data), "No User") // 앞에 있는 놈이 뒤에 있는 놈을 포함해야 OK
+
 }
 
 func TestGetUserInfo(t *testing.T) {
@@ -45,7 +47,7 @@ func TestGetUserInfo(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, res.StatusCode)
 	data, _ := ioutil.ReadAll(res.Body)
-	assert.Contains(string(data), "User Id:999")
+	assert.Contains(string(data), "No User ID:999")
 }
 
 func TestCreateUserInfo(t *testing.T) {
@@ -62,13 +64,105 @@ func TestCreateUserInfo(t *testing.T) {
 	assert.NoError(er)
 	assert.NotEqual(0, user.ID)
 
-	id := user.ID
+	id := user.ID                                                 // 만들어진 유저의 아이디로 다시 GET 호출
 	resp, errr := http.Get(ts.URL + "/users/" + strconv.Itoa(id)) // itoa = 정수를 문자열로
 	assert.NoError(errr)
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	user2 := new(User)
 	e := json.NewDecoder(resp.Body).Decode(user2)
 	assert.NoError(e)
-	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.ID, user2.ID) // 두 유저가 같다면 pass
 	assert.Equal(user.FirstName, user2.FirstName)
+}
+
+func TestDeleteUser(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler()) // 목업 테스트 서버 실행.
+	defer ts.Close()
+
+	req, _ := http.NewRequest("DELETE", ts.URL+"/users/1", nil)
+	res, err := http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+	data, _ := ioutil.ReadAll(res.Body)
+	// log.Println(string(data)) // log로 콘솔에 찍어볼 수 있음.
+	assert.Contains(string(data), "No User ID:1")
+
+	res, err = http.Post(ts.URL+"/users", "application/json", strings.NewReader(`{"FirstName": "hankyeol", "LastName":"seo", "Email":"hkseo73@gmail.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(res.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	res, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+	data, _ = ioutil.ReadAll(res.Body)
+	// log.Println(string(data)) // log로 콘솔에 찍어볼 수 있음.
+	assert.Contains(string(data), "Delete User ID:1")
+}
+
+func TestUpdateUser(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler()) // 목업 테스트 서버 실행.
+	defer ts.Close()
+
+	req, _ := http.NewRequest("PUT", ts.URL+"/users",
+		strings.NewReader(`{"ID":1, "FirstName":"updated", "LastName":"updated", "Email":"updated@naver.com"}`))
+	res, err := http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Contains(string(data), "No User ID:1") // 없으면 업데이트 안 되게 하는 정책
+
+	res, err = http.Post(ts.URL+"/users", "application/json", strings.NewReader(`{"FirstName": "hankyeol", "LastName":"seo", "Email":"hkseo73@gmail.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(res.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	updateStr := fmt.Sprintf(`{"ID":%d, "FirstName":"updated"}`, user.ID)
+	req, _ = http.NewRequest("PUT", ts.URL+"/users",
+		strings.NewReader(updateStr))
+	res, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+
+	updatedUser := new(User)
+	err = json.NewDecoder(res.Body).Decode(updatedUser)
+	assert.NoError(err)
+
+	assert.Equal(user.ID, updatedUser.ID)
+	assert.Equal("updated", updatedUser.FirstName)
+	assert.Equal(user.LastName, updatedUser.LastName)
+	assert.Equal(user.Email, updatedUser.Email)
+}
+
+func TestUsers_WithData(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler()) // 목업 테스트 서버 실행.
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL+"/users", "application/json", strings.NewReader(`{"FirstName": "hankyeol", "LastName":"seo", "Email":"hkseo73@gmail.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+
+	res, err = http.Post(ts.URL+"/users", "application/json", strings.NewReader(`{"FirstName": "hyun", "LastName":"ahn", "Email":"ahn@gmail.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+
+	res, err = http.Get(ts.URL + "/users")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+
+	users := []*User{}
+	err = json.NewDecoder(res.Body).Decode(&users)
+	assert.NoError(err)
+	assert.Equal(2, len(users))
 }
